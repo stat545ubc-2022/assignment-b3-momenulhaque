83877efbd9ae9f502ce2cdbffc12d22add6adc5e
Momenul Haque Mondol <44410127+momenulhaque@users.noreply.github.com>
2022-12-02 17:48:16 -0800

    updated for assignment 4

--- server.R
+++ server.R
@@ -1,5 +1,3 @@
-
-
 library(shiny)
 library(tidyverse)
 library(gapminder)
@@ -14,19 +12,167 @@ shinyServer(function(input, output) {
         filter((gdpPercap > input$gdpInput[1] & gdpPercap < input$gdpInput[2]) &
                  (continent %in% input$continentInput) & 
                  (year %in% input$yearInput)) 
-    })  
+    })
+  
+ ##### Table 1: summary statistics
+  lower_ci <- function(mean, se, n, conf_level = 0.95){
+    lower_ci <- mean - qt(1 - ((1 - conf_level) / 2), n - 1) * se
+  }
+  upper_ci <- function(mean, se, n, conf_level = 0.95){
+    upper_ci <- mean + qt(1 - ((1 - conf_level) / 2), n - 1) * se
+  }
+  
+ five_summary <- reactive({
+   filtered_Gapdata() %>% 
+     group_by(continent, year) %>% 
+     summarise(count = n(),
+               min = round(min(lifeExp), 2),
+               max = round(max(lifeExp), 2),
+               mean = round(mean(lifeExp, na.rm = T), 2),
+               Q1 = round(stats::quantile(lifeExp, probs = .25, na.rm = TRUE), 2),
+               se = round(sd(lifeExp, na.rm = T) / sqrt(count), 2),
+               median = round(stats::median(lifeExp, na.rm = TRUE), 2),
+               Q3 = round(stats::quantile(lifeExp, probs = .75, na.rm = TRUE), 2),
+               lower_ci = round(lower_ci(mean, se, count), 2),
+               upper_ci = round(upper_ci(mean, se, count), 2)
+               )
+ })
+
+ 
+ output$five_summary <- 
+   
+   DT::renderDataTable({
+     DT::datatable(five_summary(), options = list(
+       lengthMenu = list(c(5, 15, -1), c('5', '15', 'All')),
+       pageLength = 15
+     )) 
+     
+   })
+ 
+ output$download_Table1 <- downloadHandler(
+   
+   filename = function() {
+     paste("Summary_data", ".csv", sep="")
+   },
+   content = function(file) {
+     write.csv(five_summary(), file)
+   }
+ )
+
+ ######## Figure 1: boxplot ##############
+ 
+ 
+ plot_output <-  reactive({
+   filtered_Gapdata() %>% 
+     mutate(year = factor(year)) %>% 
+     ggplot(aes(x=lifeExp, y = year, color = year)) +
+     labs(title = paste0("Boxplot of life expectancy in ", 
+                         str_c(levels(pull(filtered_Gapdata() %>% mutate(continent=factor(continent)) %>% 
+                                             select(continent) %>% unique())), collapse = ", "),
+                         " for different years"),
+          x = "Life expectency at birth (in years)") +
+     scale_x_continuous(limits=c(0, 100)) + 
+     geom_boxplot() + theme_bw() + facet_wrap(~ continent) +
+     theme(legend.position="none",
+           strip.text = element_text(size = 12, color="black", face="bold"),
+           axis.title.x = element_text(size=12, color="black", face="bold"),
+           axis.title.y = element_text(size=12, color="black", face="bold"),
+           axis.text.y= element_text(size=10, color="black", face="bold"),
+           axis.text.x= element_text(size=10, color="black", face="bold", angle = 45, hjust = 1),
+           plot.title = element_text(size = 15, hjust = 0.5, face = "bold"),
+     )+
+     coord_flip()
+   
+ })
+ 
+ 
+ output$LifeExp_output <- 
+   
+   renderPlot({
+     
+     print(plot_output())
+     
+     
+   })
+ 
+ 
+ 
+ output$download_plot <- downloadHandler(
+   filename = "box_plot.png",
+   content = function(file) {
+     n1 = filtered_Gapdata() %>% summarise(length(unique(continent))) %>% pull()
+     png(file, width = 300*n1, height = 450)
+     print(plot_output())
+     dev.off()
+   })
+ 
+ 
+ 
+ ######## scatter plot ########## 
+ 
+ correlation_plot <- reactive({
+   filtered_Gapdata() %>% 
+     ggplot(aes(x=log(gdpPercap), y = lifeExp, color = continent)) +
+     geom_smooth(method="gam", formula = y ~ s(x, bs = "cs"), se = TRUE,
+                 aes(fill=continent)) +
+     geom_point() +
+     facet_wrap(~ continent) +
+     labs(title = paste0("Scatter plot of GDP and life expectancy"),
+          x = "Log(GDP)",
+          y = "Life expectency at birth (in years)") +
+     scale_y_continuous(limits=c(0, 100)) + 
+     theme(legend.position="right",
+           strip.text = element_text(size = 12, color="black", face="bold"),
+           axis.title.x = element_text(size=12, color="black", face="bold"),
+           axis.title.y = element_text(size=12, color="black", face="bold"),
+           axis.text.y= element_text(size=10, color="black", face="bold"),
+           axis.text.x= element_text(size=10, color="black", face="bold", angle = 45, hjust = 1),
+           plot.title = element_text(size = 15, hjust = 0.5, face = "bold"))
+   
+   
+ })
+ 
+ output$correlation_plot <- 
+   
+   renderPlot({
+     
+     correlation_plot()
+     
+     
+   })
+ 
+ output$download_correlation_plot <- downloadHandler(
+   filename = "correlation_plot.png",
+   content = function(file) {
+     png(file, width = 600, height = 450)
+     print(correlation_plot())
+     dev.off()
+   })
+ 
+ 
+ output$info1 <- renderText({
+   paste0("GDP=", input$plot_click$x, "\n",
+          "Life_exptancy=", input$plot_click$y)
+ })
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ ######## Data set 1 ##################
+
+ output$search_data <- renderText({
+   
+   n = filtered_Gapdata() %>% 
+     summarise(n=n())
+   paste0("We have found ", n, " rows for you", "\n", "\n", "\n")  
+   
+ })
   
   
-  output$search_data <- renderText({
-    
-    n = filtered_Gapdata() %>% 
-      summarise(n=n())
-    paste0("We have found ", n, " rows for you", "\n", "\n", "\n")  
-    
-  })
-  
-  
-  output$downloadData <- downloadHandler(
+  output$download_Table2 <- downloadHandler(
     
     filename = function() {
       paste("Gapminder_data", ".csv", sep="")
@@ -46,49 +192,66 @@ shinyServer(function(input, output) {
       
     })
   
-  plot_output <-  reactive({
-    filtered_Gapdata() %>% 
-      mutate(year = factor(year)) %>% 
-      ggplot(aes(x=lifeExp, y = year, color = year)) +
-      labs(title = paste0("Boxplot of life expectancy in ", 
-                          str_c(levels(pull(filtered_Gapdata() %>% mutate(continent=factor(continent)) %>% 
-                                              select(continent) %>% unique())), collapse = ", "),
-                          " for different years"),
-           x = "Life expectency at birth (in years)") +
-      geom_boxplot() + theme_bw() + facet_wrap(~ continent) +
-      theme(legend.position="none",
-            strip.text = element_text(size = 12, color="black", face="bold"),
-            axis.title.x = element_text(size=12, color="black", face="bold"),
-            axis.title.y = element_text(size=12, color="black", face="bold"),
-            axis.text.y= element_text(size=10, color="black", face="bold"),
-            axis.text.x= element_text(size=10, color="black", face="bold", angle = 45, hjust = 1),
-            plot.title = element_text(size = 15, hjust = 0.5, face = "bold"),
-      )+
-      coord_flip()
+  
+ 
+  
+ ######## Times series graphics tab ########## 
+  
+  filtered_Gapdata1 <-
+    reactive({
+      gapminder %>% 
+        filter(country %in% input$countrytInput) 
+    })
+  
+  time_Series_plot <- reactive({
+    filtered_Gapdata1() %>% 
+    ggplot(aes(x=year, y = lifeExp, color = country)) +
+    geom_point(aes(shape = country)) + geom_line(aes(linetype = country)) +
+    labs(title = paste0("Line diagram of life expectancy for ", 
+                        str_c(levels(pull(filtered_Gapdata1() %>% mutate(country=factor(country)) %>% 
+                                            select(country) %>% unique())), collapse = ", "),
+                        " from 1952 to 2007"),
+         x = "Year",
+         y = "Life expectency at birth (in years)") +
+      scale_y_continuous(limits=c(0, 100)) + 
+      scale_x_continuous(limits=c(1952, 2007), breaks = seq(1952, 2007, by = 5)) + 
+    theme(legend.position="right",
+          strip.text = element_text(size = 12, color="black", face="bold"),
+          axis.title.x = element_text(size=12, color="black", face="bold"),
+          axis.title.y = element_text(size=12, color="black", face="bold"),
+          axis.text.y= element_text(size=10, color="black", face="bold"),
+          axis.text.x= element_text(size=10, color="black", face="bold", angle = 45, hjust = 1),
+          plot.title = element_text(size = 15, hjust = 0.5, face = "bold"),
+    )
     
   })
   
-  
-  output$LifeExp_output <- 
+  output$time_series_plot <- 
     
     renderPlot({
       
-      print(plot_output())
+     time_Series_plot()
       
       
     })
   
-  
-  
-  output$download_plot <- downloadHandler(
-    filename = "box_plot.png",
+  output$download_time_plot <- downloadHandler(
+    filename = "time_series_plot.png",
     content = function(file) {
-      n1 = filtered_Gapdata() %>% summarise(length(unique(continent))) %>% pull()
-      png(file, width = 300*n1, height = 450)
-      print(plot_output())
+      png(file, width = 500, height = 450)
+      print(time_Series_plot())
       dev.off()
-    })  
+    })
   
   
-}
-)
+  output$info <- renderText({
+    paste0("year=", input$plot_click$x, "\n",
+           "Life_exptancy=", input$plot_click$y)
+  })
+ 
+   
+   
+  
+  
+  
+})
--- ui.R
+++ ui.R
@@ -2,28 +2,48 @@
 library(shiny)
 library(tidyverse)
 library(gapminder)
+library(bslib)
 
 shinyUI(fluidPage(
-  titlePanel("Life expectancy of people in continents from 1952 to 2007"),
+  theme = bs_theme(version = 4, bootswatch = "minty"),
+  titlePanel("Life expectancy of people worldwide from 1952 to 2007"),
+  p("The", strong("Gapminder"), " is a popular data set set where it contains historical
+    (1952-2007) data on various indicators, such as life expectancy, GDP, and population 
+      for countries worldwide. This app is built to explore, summarize, display the
+      life expectancy of people at different perspectives. Besides the app will
+      help to compare the life expectancy between countries or continents in different years.
+      Finally, the app will allow to explore the trend in life-expectancy over time in countries."),
   br(),
   fluidRow(
     
     column(5,
-           h4("Enter a GDP range!"), br(),
+           h4("Enter a GDP range!"),
+           p("It affects only on the", strong("Exploratory analysis"), "tab"),
+           br(),
            sliderInput("gdpInput", "gdpPercap", 200, 10000,
                        value = c(1000, 8000), pre = "$ "),
     ),
     
     column(4,
-           h4("Select the continent!"), br(),
-           selectInput("continentInput", "continent", choices = c("Africa", "Americas", "Asia", "Europe", "Oceania"),
-                       selected = c("Asia", "Europe"), multiple = TRUE, selectize = TRUE)),
+           h4("Select the continent!"),
+           p("Its selection affects only on the", strong("Exploratory analysis"), "tab"),
+           selectInput("continentInput", "continent", choices = unique(gapminder$continent),
+                       selected = c("Asia", "Europe"), multiple = TRUE, selectize = TRUE), br(),
+           
+           h4("Select the countries for times series graphs!"), 
+           p("Its selection affects only on the", strong("Time series graphics"), "tab"),
+           selectInput("countrytInput", "country", choices = unique(gapminder$country),
+                       selected = c("Canada", "Bangladesh"), multiple = TRUE, selectize = TRUE),
+           
+           
+           ),
     
     
     column(3,
-           h4("Select the year!"), br(),      
+           h4("Select the year!"), 
+           p("It affects only on the", strong("Exploratory analysis"), "tab"), br(),      
            checkboxGroupInput("yearInput", "year", choices = factor(seq(from = 1952, to = 2007, by = 5)),
-                              selected = factor(seq(from = 1952, to = 2007, by = 5)),
+                              selected = factor(c(1952, 2007)),
                               inline = FALSE)),
     
     
@@ -33,23 +53,72 @@ shinyUI(fluidPage(
   
   
   mainPanel(
+   
     fluidRow(
-      align = "center",
+      align = "left",
       tabsetPanel(
-        tabPanel("Box plot", 
+        tabPanel("Exploratory analysis", 
+                 br(), br(),
+                 h5(strong("Table 1:"),"The following table displays a summary statistics of life-expectancy
+                 among continents" ,
+                   align = "left"),
+                 br(), br(),
+                 DT::dataTableOutput("five_summary"),
+                 br(), br(), 
+                 downloadButton("download_Table1", "Click here to download Table 1", class = "butt"),
+                 tags$head(tags$style(".butt{background-color:#add8e6;} .butt{color: blue;}")),
+                 br(), br(), br(),
+                 
+                
+                
+                 h5(strong("Figure 1:"),"The following boxplot displays and compares the five number summary [minimum, Q1, median,
+                   Q3, and maximum] of life-expectancy among continents. The summary is calculated based on
+                   multiple years or a single year statistics (upon your selection)" ,
+                   align = "left"),
+                 br(), br(),
                  plotOutput("LifeExp_output"), 
                  br(), br(),
-                 downloadButton('download_plot',label='Download the plot', class = "butt"),
+                 downloadButton('download_plot',label='Download Figure 1', class = "butt"),
                  tags$head(tags$style(".butt{background-color:#add8e6;} .butt{color: blue;}")),
-                 br(), br(), br()),
+                 br(), br(), br(),
         
-        tabPanel("Data set", 
-                 textOutput("search_data"),
-                 DT::dataTableOutput("data_table"),
+
+                 h5(strong("Figure 2:"),"Correlation between GDP and Life expectancy in continents. " ,
+                    align = "left"),
                  br(), br(),
-                 downloadButton("downloadData", "Click here to download the data", class = "butt"),
+                 plotOutput("correlation_plot", click = "plot_click"),
+                 verbatimTextOutput("info1"), 
+                 br(), br(),
+                 downloadButton('download_correlation_plot',label='Download Figure 2', class = "butt"),
                  tags$head(tags$style(".butt{background-color:#add8e6;} .butt{color: blue;}")),
-                 br(), br(), br())
+                 br(), br(), br(),
+                 
+                 
+                 
+                                  
+               h5(strong("Data set 1:"),"The following table displays the data set searched" ,
+                  align = "left"),
+               br(), br(), 
+                textOutput("search_data"),
+                DT::dataTableOutput("data_table"),
+                br(), br(),
+                downloadButton("download_Table2", "Click here to download Data set 1", class = "butt"),
+                tags$head(tags$style(".butt{background-color:#add8e6;} .butt{color: blue;}")),
+                br(), br(), br()),
+        
+        tabPanel("Times series graphics",
+                 h5(strong("Figure 3:"),"The following line plot displays and compares the yearly life-expectancy
+                 among countries. Click on the graph to see the coordinate." ,
+                    align = "left"),
+                 br(), br(),
+                 plotOutput("time_series_plot", click = "plot_click"),
+                 verbatimTextOutput("info"),
+                 br(), br(),
+                 downloadButton('download_time_plot',label='Download Figure 3', class = "butt"),
+                 tags$head(tags$style(".butt{background-color:#add8e6;} .butt{color: blue;}")),
+                 br(), br(), br(),
+                 
+        )
       )
     )
   )
